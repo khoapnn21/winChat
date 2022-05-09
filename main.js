b07ce9241dc66099ad692c068cd550fb41bc8b5f
KhoaPham <pnnk2013@gmail.com>
2022-05-09 15:12:38 +0700

    update 9/5

--- main.js
+++ main.js
@@ -1,22 +1,64 @@
-function sendMess() {
-    let contentMess = document.getElementById("textMess").value
-    if(contentMess === '') return;
-    
-    let newWin = window.open("https://khoapnn21.github.io/winChat/subIndex", "subIndex")
-    newWin.postMessage(contentMess, "https://khoapnn21.github.io/winChat/subIndex");	
+let message = localStorage.getItem('data') ?? '{}';
+let storeMess = Object.assign(JSON.parse(message)) ;
+let displayMsg = document.getElementById('displayMsg')
+let screen = document.getElementById('screen')
+let submit = document.getElementById('submit')
+let input = document.getElementById('textMess')
+input.focus()
+let nameWin = window.name
+let deleteStorage = document.getElementById('delete')
 
 
-};
 
-window.addEventListener("message", parentDisplay,false );
 
-function parentDisplay(e) {
-    if (e.origin !== "https://khoapnn21.github.io/winChat") return;
-    document.getElementById("displayMsg").innerHTML += e.data  ;
-    
+
+function getMess() {
+    let storage = JSON.parse(localStorage.getItem('data'))
+    for(let key in storage) {
+        renderMess(storage[key][0], storage[key][1])
+    }    
+    scrollBottom()
 }
- 
+getMess()
 
+function renderMess(value, nameWin) {
+    if(nameWin == 'index') {
+        displayMsg.innerHTML +=`<br> <span style='background: blue; float: right;'>${value}</span>` ;   
 
+    }else {
+        displayMsg.innerHTML +=`<br> <span style='background: yellow; float: left;'>${value}</span>` ;   
 
+    }    
+}
 
+function setMess() {
+    let eValue = input.value
+    if(eValue === '') return;
+
+    storeMess[Object.keys(storeMess).length] = [eValue, nameWin]   
+    localStorage.setItem('data', JSON.stringify(storeMess))    
+    renderMess(eValue, nameWin)
+    sendMess(eValue)
+}
+
+submit.addEventListener('click', setMess)
+
+function scrollBottom() {
+    screen.scrollTop = screen.scrollHeight;
+}
+
+deleteStorage.onclick = function() {
+    localStorage.clear();
+    location.reload()
+}
+
+function sendMess(value) {
+    let newWin = window.open('https://khoapnn21.github.io/winChat/subIndex', 'subIndex')    
+    newWin.postMessage(value, 'https://khoapnn21.github.io/winChat/subIndex')
+}
+
+window.addEventListener('message', function(e) {
+    if(e.origin != 'https://khoapnn21.github.io/winChat') return
+    renderMess(e.data, nameWin)
+    scrollBottom()
+});
--- submain.js
+++ submain.js
@@ -1,20 +1,58 @@
-
-window.addEventListener("message", subDisplay,false );
-
-function subDisplay(e) {
-    
-    if (e.origin !== "https://khoapnn21.github.io/winChat") return;
-    document.getElementById("subDisplayMsg").innerHTML += e.data ;
-    
+let message = localStorage.getItem('data') ?? '{}';
+let storeMess = Object.assign(JSON.parse(message)) ;
+let displayMsg = document.getElementById('subDisplayMsg')
+let screen = document.getElementById('subsCreen')
+let submit = document.getElementById('subSubmit')
+let input = document.getElementById('subTextMess')
+input.focus()
+let nameWin = window.name
+let deleteStorage = document.getElementById('subDelete')
+function scrollBottom() {
+    screen.scrollTop = screen.scrollHeight
 }
 
-function sendMess() {
-    let contentMess = document.getElementById("subTextMess").value
-    if(contentMess === '') return;
-    
-    let newWin = window.open("https://khoapnn21.github.io/winChat/index", "index")
-    newWin.postMessage(contentMess, "https://khoapnn21.github.io/winChat/index");	
-};
-    
+function getMess() {
+    let storage = JSON.parse(localStorage.getItem('data'))
+    for(let key in storage) {
+        renderMess( storage[key][0], storage[key][1])
+    }    
+    scrollBottom()
+}
+getMess()
 
-    
+function renderMess(value, nameWin) {
+    if(nameWin == 'index') {
+        displayMsg.innerHTML +=`<br> <span style='background: yellow; float: left;'>${value}</span>` ;   
+    }else {
+        displayMsg.innerHTML +=`<br> <span style='background: blue; float: right;'>${value}</span>` ;   
+
+    }  
+}
+
+function setMess() {
+    let eValue = input.value
+    if(eValue === '') return;
+    storeMess[Object.keys(storeMess).length] = [eValue, nameWin ]   
+    localStorage.setItem('data', JSON.stringify(storeMess))   
+    renderMess(eValue, nameWin)
+    sendMess(eValue)
+}
+
+submit.addEventListener('click', setMess)
+
+deleteStorage.onclick = function() {
+    localStorage.clear();
+    location.reload()
+}
+
+function sendMess(value) {  
+    let newWin = window.open('https://khoapnn21.github.io/winChat/index', 'index')    
+    newWin.postMessage(value, 'https://khoapnn21.github.io/winChat/index')
+
+}
+
+window.addEventListener('message', function(e) {
+    if(e.origin != 'https://khoapnn21.github.io/winChat') return
+    renderMess(e.data, nameWin)
+    scrollBottom()
+});
